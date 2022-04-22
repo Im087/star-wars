f4e4c93ae33bb982779c7f4274423a7e4b5b3aaf
D.Lin <lindaqi087@gmail.com>
2022-04-22 22:08:45 +0200

    add carousel to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="home-container">
-    home
+  <div class="home-container d-flex flex-column justify-content-around">
+    <b-carousel id="home-carousel" :interval="4000" controls indicators>
+      <b-carousel-slide img-src="@/assets/img/star-wars-Macbook-Wallpaper.jpg">
+      </b-carousel-slide>
+      <b-carousel-slide img-src="@/assets/img/star-wars-pc-Wallpaper.jpg">
+      </b-carousel-slide>
+      <b-carousel-slide img-src="@/assets/img/star-wars-Wallpaper.jpg">
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 
