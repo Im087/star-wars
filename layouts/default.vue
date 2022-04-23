d5bebd7a7cae163632079fac76b6aace7d26d647
D.Lin <lindaqi087@gmail.com>
2022-04-24 00:46:22 +0200

    add page transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="layout-container d-flex flex-column justify-content-between">
     <Header />
-    <Nuxt class="flex-fill" keep-alive />
+    <transition name="fade">
+      <Nuxt class="flex-fill" keep-alive />
+    </transition>
     <Footer />
   </div>
 </template>
@@ -22,5 +24,12 @@ export default {
 <style lang="scss" scoped>
 .layout-container {
   height: 100%;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
